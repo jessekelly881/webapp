f5d77e043e3b8781653498dc7f362ac03926bdd4
Jesse Kelly <kellyjesse881@gmail.com>
2022-05-12 17:03:40 -0500

    chore(plugins): added relative imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import lib from 'lib';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-      PD Template
+      { lib() }
   </React.StrictMode>
 )
