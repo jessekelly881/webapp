fd177e784fef003d36cf13eefafe2c030b477525
Jesse Kelly <kellyjesse881@gmail.com>
2022-06-30 21:54:02 +0200

    feat: added local-storage code

--- src/app/state/index.ts
+++ src/app/state/index.ts
@@ -1,15 +1,17 @@
 import * as A from 'fp-ts/Array';
 import * as NEA from 'fp-ts/NonEmptyArray';
 import * as S from 'io-ts/Schema';
-import * as s from 'fp-ts/string';
+import * as Eq from 'io-ts/Eq';
+import * as D from 'io-ts/Decoder';
 import * as Semi from 'fp-ts/Semigroup';
 import { pipe } from 'fp-ts/function';
+export { getLocalState } from './localstorage';
+
 
 /*
  * State from multiple origins should be merged using the state semigroup.
  * State may come from localStorage, config files, etc.
  */
-
 
 interface State {
     name?: string;
@@ -21,13 +23,21 @@ export const State: S.Schema<State> = S.make((S) =>
   })
 )
 
+export const decoder = S.interpreter(D.Schemable)(State);
+export const eq = S.interpreter(Eq.Schemable)(State);
+
+
 export const semigroup = Semi.struct<State>({
   name: Semi.last()
 })
 
+/*
+ * default application state
+ */
 export const defaultState: State = {
   name: ""
 }
+
 
 export const mergeState = (arr: State[]) => pipe(
   A.prepend(defaultState)(arr),
