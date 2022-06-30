f772e1a528a86ca6295716153db3b5721f7bb71f
Jesse Kelly <kellyjesse881@gmail.com>
2022-06-30 21:54:02 +0200

    feat: added local-storage code

---
+++ src/app/state/localstorage.ts
@@ -0,0 +1,28 @@
+import { getItem } from 'fp-ts-local-storage'
+import * as IOE from 'fp-ts/lib/IOEither'
+import * as E from 'fp-ts/Either'
+import * as O from 'fp-ts/Option'
+import { pipe, flow } from 'fp-ts/function'
+import * as J from 'fp-ts-std/JSON';
+import * as S from '.';
+
+const key = "state";
+
+/*
+ * Get local (partial) copy of state stored in local-storage
+ * TODO: Needs refactoring
+ */
+export const getLocalState = pipe(
+    IOE.tryCatch(getItem(key), E.toError),
+    IOE.map(flow(
+        O.getOrElse(() => ""),
+        J.parse(E.toError),
+        IOE.fromEither,
+    )),
+    IOE.flatten,
+    IOE.map(flow(
+        S.decoder.decode,
+        IOE.fromEither,
+    )),
+    IOE.flattenW,
+)
