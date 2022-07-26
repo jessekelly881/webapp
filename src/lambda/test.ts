dc3da411ddb0dda23c66275f67c0159b3f144126
Jesse Kelly <kellyjesse881@gmail.com>
2022-07-26 19:28:36 +0200

    feat: updated lamda response format

--- src/lambda/test.ts
+++ src/lambda/test.ts
@@ -4,6 +4,9 @@ import lib from 'lib';
 const handler: Handler = async () => ({
     statusCode: 200,
     body: JSON.stringify({ message: lib() }),
+    headers: {
+        "Content-Type": "application/json; charset=utf-8"
+    }
 });
 
 export { handler };
