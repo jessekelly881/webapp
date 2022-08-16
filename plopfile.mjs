9ad6da987a3fbd962beae87053034b4c25bb3a79
Jesse Kelly <kellyjesse881@gmail.com>
2022-08-16 17:56:30 +0200

    chore: added plop

---
+++ plopfile.mjs
@@ -0,0 +1,19 @@
+export default (plop) => {
+  plop.setGenerator("schema", {
+    description: "io-ts schema",
+    prompts: [
+      {
+        type: "input",
+        name: "name",
+        message: "schema name please",
+      },
+    ],
+    actions: [
+      {
+        type: "add",
+        path: "src/lib/types/{{name}}/index.ts",
+        templateFile: ".templates/schema.hbs",
+      },
+    ],
+  });
+};
