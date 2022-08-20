ae796140e000447b21532ce9c897238e3230de23
Jesse Kelly <kellyjesse881@gmail.com>
2022-08-20 09:53:13 +0200

    feat: added amplitude

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from 'app';
 import { createBrowserHistory } from 'history';
+import * as Amp from '@amplitude/analytics-browser';
 import "./index.css"
+
+
+/*
+ * Amplitude
+ * TODO: Refactor
+ */
+const amplifyKey = process.env?.AMPLITUDE_KEY;
+if (amplifyKey) {
+    Amp.init(amplifyKey);
+    Amp.track('Init');
+}
 
 const history = createBrowserHistory();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-      <App history={history} />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App history={history} />
+    </React.StrictMode>
 )
-
