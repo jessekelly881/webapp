4505b135c598c98753d95d3f8c672c62caf53702
Jesse Kelly <kellyjesse881@gmail.com>
2022-10-16 21:22:49 +0200

    chore: added vite-handlebars

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import progress from 'vite-plugin-progress'
+import progress from 'vite-plugin-progress';
+import handlebars from 'vite-plugin-handlebars';
 
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    handlebars({
+      context: {
+        title: "Template",
+      },
+    }),
     react(),
     tsconfigPaths(),
-    progress()
+    progress(),
   ],
   server: {
     host: true
