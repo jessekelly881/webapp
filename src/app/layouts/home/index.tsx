36df777aee029fe3cff60acd61f5a659fa5b5f44
Jesse Kelly <kellyjesse881@gmail.com>
2022-10-22 16:29:56 +0200

    feat: replace react-icons with unplugin-icons

--- src/app/layouts/home/index.tsx
+++ src/app/layouts/home/index.tsx
@@ -1,18 +1,21 @@
 import { Route } from 'app/router';
 import React from 'react';
+import Github from '~icons/simple-icons/github';
+
 
 
 interface LayoutP {
-    setRoute: (r: Route) => void;
+	setRoute: (r: Route) => void;
 }
 
 const Home = ({ setRoute }: LayoutP) => (
-    <>
-        <span>Home</span>
-        <button onClick={() => setRoute({ type: "Test" })} type="button">
-            Test
-        </button>
-    </>
+	<>
+		<span>Home</span>
+		<Github />
+		<button onClick={() => setRoute({ type: "Test" })} type="button">
+			Test
+		</button>
+	</>
 )
 
 export default Home;
--- vite.config.ts
+++ vite.config.ts
@@ -13,65 +13,72 @@ import { Schema, ValidateEnv } from "@julr/vite-plugin-validate-env";
 import config from "./src/config";
 import removeConsole from "vite-plugin-remove-console";
 import Inspect from 'vite-plugin-inspect'
+import Icons from 'unplugin-icons/vite'
 
 
 export default defineConfig({
-  plugins: [
-    handlebars({
-      context: {
-        title: config.name,
-        description: config.description
-      },
-    }),
-    react(),
-    removeConsole(),
-    tsconfigPaths(),
-    progress(),
-    qrcode(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: config.name,
-        short_name: config.name,
-        description: config.description,
-        icons: [
-          {
-            src: 'android-chrome-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'android-chrome-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          },
-        ]
-      },
-      devOptions: {
-        enabled: true
-      },
-    }),
-    visualizer({ // last
-      emitFile: true,
-      gzipSize: true,
-      brotliSize: true,
-    }),
-    ValidateEnv({
-      // Verifies that an env variable exists and validates type. E.g.
-      // A_KEY: Schema.string()
-    }),
-    Inspect()
-  ],
-  server: {
-    open: true,
-    host: true
-  },
-  define: {
-    'process.env': process.env
-  },
-  test: {
-    watch: false,
-    globals: true,
-    environment: 'jsdom'
-  },
+	plugins: [
+		handlebars({
+			context: {
+				title: config.name,
+				description: config.description
+			},
+		}),
+		Icons({
+			// experimental
+			compiler: 'jsx',
+			jsx: 'react',
+			autoInstall: true,
+		}),
+		react(),
+		removeConsole(),
+		tsconfigPaths(),
+		progress(),
+		qrcode(),
+		VitePWA({
+			registerType: 'autoUpdate',
+			manifest: {
+				name: config.name,
+				short_name: config.name,
+				description: config.description,
+				icons: [
+					{
+						src: 'android-chrome-192x192.png',
+						sizes: '192x192',
+						type: 'image/png'
+					},
+					{
+						src: 'android-chrome-512x512.png',
+						sizes: '512x512',
+						type: 'image/png'
+					},
+				]
+			},
+			devOptions: {
+				enabled: true
+			},
+		}),
+		visualizer({ // last
+			emitFile: true,
+			gzipSize: true,
+			brotliSize: true,
+		}),
+		ValidateEnv({
+			// Verifies that an env variable exists and validates type. E.g.
+			// A_KEY: Schema.string()
+		}),
+		Inspect()
+	],
+	server: {
+		open: true,
+		host: true
+	},
+	define: {
+		'process.env': process.env
+	},
+	test: {
+		watch: false,
+		globals: true,
+		environment: 'jsdom'
+	},
 })
